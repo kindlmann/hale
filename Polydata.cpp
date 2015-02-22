2ff80b50313ac7edcd1ada33ec74b60a03c03734
Gordon Kindlmann <glk@uchicago.edu>
2015-02-21 23:04:57 -0600

    now with working polydata?

---
+++ Polydata.cpp
@@ -0,0 +1,180 @@
+/*
+  hale: support for minimalist scientific visualization
+  Copyright (C) 2014  University of Chicago
+
+  This software is provided 'as-is', without any express or implied
+  warranty. In no event will the authors be held liable for any damages
+  arising from the use of this software. Permission is granted to anyone to
+  use this software for any purpose, including commercial applications, and
+  to alter it and redistribute it freely, subject to the following
+  restrictions:
+
+  1. The origin of this software must not be misrepresented; you must not
+  claim that you wrote the original software. If you use this software in a
+  product, an acknowledgment in the product documentation would be
+  appreciated but is not required.
+
+  2. Altered source versions must be plainly marked as such, and must not be
+  misrepresented as being the original software.
+
+  3. This notice may not be removed or altered from any source distribution.
+*/
+
+#include "Hale.h"
+#include "privateHale.h"
+
+namespace Hale {
+
+void
+Polydata::_buffer(bool newaddr) {
+  // static const char me[]="Hale::Polydata::_buffer";
+  const limnPolyData *lpd = this->lpld();
+  unsigned int ibits = limnPolyDataInfoBitFlag(lpd);
+
+  glBindBuffer(GL_ARRAY_BUFFER, _buff[_buffIdx[vertAttrIdxXYZW]]);
+  // printf("#""glBindBuffer(GL_ARRAY_BUFFER, %u);\n", _buff[_buffIdx[vertAttrIdxXYZW]]);
+  if (newaddr) {
+    glBufferData(GL_ARRAY_BUFFER, lpd->xyzwNum*sizeof(float)*4, lpd->xyzw, GL_DYNAMIC_DRAW);
+    // printf("#""glBufferData(GL_ARRAY_BUFFER, %u, lpd->xyzw, GL_DYNAMIC_DRAW);\n", (unsigned int)(lpd->xyzwNum*sizeof(float)*4));
+  } else {
+    glBufferSubData(GL_ARRAY_BUFFER, 0, lpd->xyzwNum*sizeof(float)*4, lpd->xyzw);
+  }
+  glVertexAttribPointer(Hale::vertAttrIdxXYZW, 4, GL_FLOAT, GL_FALSE, 0, 0);
+  // printf("#""glVertexAttribPointer(%u, 4, GL_FLOAT, GL_FALSE, 0, 0);\n", Hale::vertAttrIdxXYZW);
+
+  if (ibits & (1 << limnPolyDataInfoNorm)) {
+    glBindBuffer(GL_ARRAY_BUFFER, _buff[_buffIdx[vertAttrIdxNorm]]);
+    // printf("#""glBindBuffer(GL_ARRAY_BUFFER, %u);\n", _buff[_buffIdx[vertAttrIdxNorm]]);
+    if (newaddr) {
+      glBufferData(GL_ARRAY_BUFFER, lpd->normNum*sizeof(float)*3, lpd->norm, GL_DYNAMIC_DRAW);
+      // printf("#""glBufferData(GL_ARRAY_BUFFER, %u, lpd->norm, GL_DYNAMIC_DRAW);\n", (unsigned int)(lpd->normNum*sizeof(float)*3));
+    } else {
+      glBufferSubData(GL_ARRAY_BUFFER, 0, lpd->normNum*sizeof(float)*3,lpd->norm);
+    }
+    glVertexAttribPointer(Hale::vertAttrIdxNorm, 3, GL_FLOAT,GL_FALSE, 0, 0);
+    // printf("#""glVertexAttribPointer(%u, 3, GL_FLOAT, GL_FALSE, 0, 0);\n", Hale::vertAttrIdxNorm);
+  }
+
+  if (ibits & (1 << limnPolyDataInfoRGBA)) {
+    glBindBuffer(GL_ARRAY_BUFFER, _buff[_buffIdx[vertAttrIdxRGBA]]);
+    // printf("#""glBindBuffer(GL_ARRAY_BUFFER, %u);\n", _buff[_buffIdx[vertAttrIdxRGBA]]);
+    if (newaddr) {
+      glBufferData(GL_ARRAY_BUFFER, lpd->rgbaNum*sizeof(char)*4, lpd->rgba, GL_DYNAMIC_DRAW);
+      // printf("#""glBufferData(GL_ARRAY_BUFFER, %u, lpd->rgba, GL_DYNAMIC_DRAW);\n", (unsigned int)(lpd->rgbaNum*sizeof(char)*4));
+    } else {
+      glBufferSubData(GL_ARRAY_BUFFER, 0, lpd->rgbaNum*sizeof(char)*4,lpd->rgba);
+    }
+    glVertexAttribPointer(Hale::vertAttrIdxRGBA, 4, GL_UNSIGNED_BYTE, GL_TRUE, 0, 0);
+    // printf("#""glVertexAttribPointer(%u, 4, GL_UNSIGNED_BYTE, GL_TRUE, 0, 0);\n", Hale::vertAttrIdxRGBA);
+  }
+
+  /* HEY: tang and tex2 */
+
+
+  if (!_elms || newaddr) {
+    if (_elms) {
+      glDeleteBuffers(1, &_elms);
+      // printf("#""glDeleteBuffers(1, %u);\n", _elms);
+    }
+    glGenBuffers(1, &_elms);
+    // printf("#""glGenBuffers(1, &); -> %u\n", _elms);
+    glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, _elms);
+    // printf("#""glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, %u);\n", _elms);
+    glBufferData(GL_ELEMENT_ARRAY_BUFFER,
+                 lpd->indxNum * sizeof(unsigned int),
+                 lpd->indx, GL_DYNAMIC_DRAW);
+    // printf("#""glBufferData(GL_ELEMENT_ARRAY_BUFFER, %u, lpd->indx, GL_DYNAMIC_DRAW);\n", (unsigned int)(lpd->indxNum * sizeof(unsigned int)));
+  }
+  return;
+}
+
+void
+Polydata::_init() {
+  // static const char me[]="Hale::Polydata::_init";
+  const limnPolyData *lpld = this->lpld();
+  unsigned int aa, ibits = limnPolyDataInfoBitFlag(lpld);
+  _buffNum = 1 + airBitsSet(ibits);   /* lpld->xyzw is always set */
+  // printf("%s: %p|%p %u buffers to set\n", me, _lpld, _lpldOwn, _buffNum);
+  _buff = AIR_CALLOC(_buffNum, GLuint);
+  glGenBuffers(_buffNum, _buff);
+  // printf("#""glGenBuffers(%u, &); -> %u %u %u\n", _buffNum, _buff[0], _buff[1], _buff[2]);
+  glGenVertexArrays(1, &_vao);
+  // printf("#""glGenVertexArrays(1, &); -> %u\n", _vao);
+  glBindVertexArray(_vao);
+  // printf("#""glBindVertexArray(%u);\n", _vao);
+
+  aa = 0;
+  glEnableVertexAttribArray(Hale::vertAttrIdxXYZW);
+  // printf("#""glEnableVertexAttribArray(%u);\n", Hale::vertAttrIdxXYZW);
+  _buffIdx[Hale::vertAttrIdxXYZW] = aa++;
+  for (int ii=limnPolyDataInfoUnknown+1; ii<limnPolyDataInfoLast; ii++) {
+    /* HEY assumption of limnPolyDataInfo, Hale::vertAttrIdx mirroring */
+    int hva = ii - limnPolyDataInfoRGBA + Hale::vertAttrIdxRGBA;
+    if (ibits & (1 << ii)) {
+      glEnableVertexAttribArray(hva);
+      // printf("#""glEnableVertexAttribArray(%u);\n", hva);
+      _buffIdx[hva] = aa++;
+    } else {
+      _buffIdx[hva] = -1;
+    }
+  }
+  _elms = 0;
+  _buffer(true);
+  return;
+}
+
+Polydata::Polydata(const limnPolyData *poly) {
+
+  _lpld = poly;
+  _lpldOwn = NULL;
+  _init();
+}
+
+Polydata::Polydata(limnPolyData *poly, bool own) {
+
+  if (own) {
+    _lpld = NULL;
+    _lpldOwn = poly;
+  } else {
+    _lpld = poly;
+    _lpldOwn = NULL;
+  }
+  _init();
+}
+
+Polydata::~Polydata() {
+  // static const char me[]="Hale::Polydata::~Polydata";
+
+  if (_lpldOwn) {
+    limnPolyDataNix(_lpldOwn);
+  }
+  glDeleteVertexArrays(1, &_vao);
+  glDeleteBuffers(1, &_elms);
+  glDeleteBuffers(_buffNum, _buff);
+  free(_buff);
+}
+
+void
+Polydata::draw() {
+  static const char me[]="Hale::Polydata::draw";
+
+  glBindVertexArray(_vao);
+  // printf("#""glBindVertexArray(%u);\n", _vao);
+  /* ? needed with every render call ? */
+  glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, _elms);
+  // printf("#""glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, %u);\n", _elms);
+
+  const limnPolyData *lpld = this->lpld();
+  int offset = 0;
+  for (unsigned int ii=0; ii<lpld->primNum; ii++) {
+    glDrawElements(Hale::limnToGLPrim(lpld->type[ii]),
+                   lpld->icnt[ii],
+                   GL_UNSIGNED_INT, ((void*) 0));
+    // printf("#""glDrawElements(%u, %u, GL_UNSIGNED_INT, 0);\n", Hale::limnToGLPrim(lpld->type[ii]), lpld->icnt[ii]);
+    Hale::glErrorCheck(me, "glDrawElements(prim " + std::to_string(ii) + ")");
+    offset += lpld->icnt[ii];
+  }
+  return;
+}
+
+} // namespace Hale
