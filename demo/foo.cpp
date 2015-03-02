5f8b6f5c209034aedfb2e1ca9a3fb72f11c6bae4
Gordon Kindlmann <glk@uchicago.edu>
2015-03-02 04:43:14 -0600

    various tweaks, and now each Polydata has its own program

---
+++ demo/foo.cpp
@@ -0,0 +1,161 @@
+#include <iostream>
+#include <Hale.h>
+#include <glm/glm.hpp>
+
+void render(Hale::Viewer *viewer){
+  Hale::uniform("projectMat", viewer->camera.project());
+  Hale::uniform("viewMat", viewer->camera.view());
+
+  viewer->draw();
+  viewer->bufferSwap();
+}
+
+int
+main(int argc, const char **argv) {
+  const char *me;
+  char *err;
+  hestOpt *hopt=NULL;
+  hestParm *hparm;
+  airArray *mop;
+
+  /* variables learned via hest */
+  Nrrd *nin;
+  float camfr[3], camat[3], camup[3], camnc, camfc, camFOV;
+  int camortho;
+  unsigned int camsize[2];
+  double isovalue, sliso, isomin, isomax;
+
+  /* boilerplate hest code */
+  me = argv[0];
+  mop = airMopNew();
+  hparm = hestParmNew();
+  airMopAdd(mop, hparm, (airMopper)hestParmFree, airMopAlways);
+  /* setting up the command-line options */
+  hparm->respFileEnable = AIR_TRUE;
+  hestOptAdd(&hopt, "i", "volume", airTypeOther, 1, 1, &nin, NULL,
+             "input volume to isosurface", NULL, NULL, nrrdHestNrrd);
+  hestOptAdd(&hopt, "v", "isovalue", airTypeDouble, 1, 1, &isovalue, "nan",
+             "isovalue at which to run Marching Cubes");
+  hestOptAdd(&hopt, "fr", "x y z", airTypeFloat, 3, 3, camfr, "3 4 5",
+             "look-from point");
+  hestOptAdd(&hopt, "at", "x y z", airTypeFloat, 3, 3, camat, "0 0 0",
+             "look-at point");
+  hestOptAdd(&hopt, "up", "x y z", airTypeFloat, 3, 3, camup, "0 0 1",
+             "up direction");
+  hestOptAdd(&hopt, "nc", "dist", airTypeFloat, 1, 1, &(camnc), "-2",
+             "at-relative near clipping distance");
+  hestOptAdd(&hopt, "fc", "dist", airTypeFloat, 1, 1, &(camfc), "2",
+             "at-relative far clipping distance");
+  hestOptAdd(&hopt, "fov", "angle", airTypeFloat, 1, 1, &(camFOV), "20",
+             "vertical field-of-view, in degrees. Full vertical "
+             "extent of image plane subtends this angle.");
+  hestOptAdd(&hopt, "sz", "s0 s1", airTypeUInt, 2, 2, &(camsize), "640 480",
+             "# samples (horz vert) of image plane. ");
+  hestOptAdd(&hopt, "ortho", NULL, airTypeInt, 0, 0, &(camortho), NULL,
+             "use orthographic instead of (the default) "
+             "perspective projection ");
+
+  hestParseOrDie(hopt, argc-1, argv+1, hparm,
+                 me, "demo program", AIR_TRUE, AIR_TRUE, AIR_TRUE);
+  airMopAdd(mop, hopt, (airMopper)hestOptFree, airMopAlways);
+  airMopAdd(mop, hopt, (airMopper)hestParseFree, airMopAlways);
+
+  /* learn value range, and set initial isovalue if needed */
+  NrrdRange *range = nrrdRangeNewSet(nin, AIR_FALSE);
+  airMopAdd(mop, range, (airMopper)nrrdRangeNix, airMopAlways);
+  isomin = range->min;
+  isomax = range->max;
+  if (!AIR_EXISTS(isovalue)) {
+    isovalue = (isomin + isomax)/2;
+  }
+
+  /* first, make sure we can isosurface ok */
+  limnPolyData *lpld = limnPolyDataNew();
+  seekContext *sctx = seekContextNew();
+  airMopAdd(mop, sctx, (airMopper)seekContextNix, airMopAlways);
+  sctx->pldArrIncr = nrrdElementNumber(nin);
+  seekVerboseSet(sctx, 0);
+  seekNormalsFindSet(sctx, AIR_TRUE);
+  if (seekDataSet(sctx, nin, NULL, 0)
+      || seekTypeSet(sctx, seekTypeIsocontour)
+      || seekIsovalueSet(sctx, isovalue)
+      || seekUpdate(sctx)
+      || seekExtract(sctx, lpld)) {
+    airMopAdd(mop, err=biffGetDone(SEEK), airFree, airMopAlways);
+    fprintf(stderr, "trouble with isosurfacing:\n%s", err);
+    airMopError(mop);
+    return 1;
+  }
+  if (!lpld->xyzwNum) {
+    fprintf(stderr, "%s: warning: No isocontour generated at isovalue %g\n",
+            me, isovalue);
+  }
+
+  /* then create empty scene */
+  Hale::init();
+  Hale::Scene scene;
+  /* then create viewer (in order to create the OpenGL context) */
+  Hale::Viewer viewer(camsize[0], camsize[1], "Iso", &scene);
+  viewer.lightDir(glm::vec3(-1.0f, 1.0f, 3.0f));
+  viewer.camera.init(glm::vec3(camfr[0], camfr[1], camfr[2]),
+                     glm::vec3(camat[0], camat[1], camat[2]),
+                     glm::vec3(camup[0], camup[1], camup[2]),
+                     camFOV, (float)camsize[0]/camsize[1],
+                     camnc, camfc, camortho);
+  viewer.refreshCB((Hale::ViewerRefresher)render);
+  viewer.refreshData(&viewer);
+  sliso = isovalue;
+  viewer.slider(&sliso, isomin, isomax);
+  viewer.current();
+
+  /* then add it to scene */
+  /* HOTE: Hale bug prevents a different preprogram from being used; seems
+     that all objects have to use same program */
+  limnPolyData *lcube = limnPolyDataNew();
+  limnPolyDataCubeTriangles(lcube, 1 << limnPolyDataInfoNorm, AIR_TRUE);
+  Hale::Polydata hcube(lcube, true,
+                       Hale::ProgramLib(Hale::preprogramAmbDiff2SideSolid));
+  hcube.colorSolid(1,0.5,0.5);
+  hcube.model(glm::transpose(glm::mat4(30.0f, 0.0f, 0.0f, 0.0f,
+                                       0.0f, 30.0f, 0.0f, 0.0f,
+                                       0.0f, 0.0f, 0.5f, 0.0f,
+                                       0.0f, 0.0f, 0.0f, 1.0f)));
+  scene.add(&hcube);
+
+  limnPolyData *lball = limnPolyDataNew();
+  limnPolyDataSpiralSphere(lball, 1 << limnPolyDataInfoNorm, 20, 20);
+  Hale::Polydata hball(lball, true,
+                       Hale::ProgramLib(Hale::preprogramAmbDiff2SideSolid));
+  hball.colorSolid(0.5,1.0,0.5);
+  hball.model(glm::transpose(glm::mat4(30.0f, 0.0f, 0.0f, 0.0f,
+                                       0.0f, 30.0f, 0.0f, 60.0f,
+                                       0.0f, 0.0f, 30.0f, 0.0f,
+                                       0.0f, 0.0f, 0.0f, 1.0f)));
+  scene.add(&hball);
+
+  /* NOTE: Hale has some bug that prevents the "scene.add(&hcube);"
+     from being after the "scene.add(&hply);" below */
+  Hale::Polydata hply(lpld, true,  // hply now owns lpld
+                      Hale::ProgramLib(Hale::preprogramAmbDiff2SideSolid));
+  scene.add(&hply);
+
+
+  scene.drawInit();
+  while(!Hale::finishing){
+    glfwWaitEvents();
+    if (viewer.sliding() && sliso != isovalue) {
+      isovalue = sliso;
+      printf("%s: isosurfacing at %g\n", me, isovalue);
+      seekIsovalueSet(sctx, isovalue);
+      seekUpdate(sctx);
+      seekExtract(sctx, lpld);
+      hply.rebuffer();
+    }
+    render(&viewer);
+  }
+
+  /* clean exit; all okay */
+  Hale::done();
+  airMopOkay(mop);
+  return 0;
+}
