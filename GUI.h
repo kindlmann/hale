5eeacb04db8154684dd0df05fe8a091c4776a001
Ashwin Chetty <ashwinchetty@gmail.com>
2016-06-04 14:08:35 -0500

    gui skeleton

--- GUI.h
+++ GUI.h
@@ -7,71 +7,64 @@
 
 
 
-/*
-
-    GUIHandlers may differ in three ways:
-        1. type of value.
-        2. GUI-mode of interaction.
-        3. Manner of update.
-
-*/
-
-/*
- * Can display:
- *   - enum, int, double, string
- *
- */
-
-// T is type of variable.
-// W is type of window.
-
-
 class HaleGUI;
 template<class T>
 class VariableBinding;
+class GenericGUIElement;
+template<class W, class T>
+class GUIElement;
 
 
-
-
+/*
+ * A wrapper over a particular variable. Allows a consistent means
+ * of getting/setting data from the GUI and otherwise.
+ */
 template<class T>
 class VariableBinding{
 public:
-    typedef T (*t_getter)();
-    typedef void (*t_setter)(T);
+  typedef T (*t_getter)();
+  typedef void (*t_setter)(T);
 protected:
-    T* value;         // pointer to real value, if a setter/getter are not used.
-    t_getter getter;  // the method which gets the value (wherever it may be).
-    t_setter setter;  // the method which sets the value.
-    bool changed;
+  T* value;         // pointer to real value, if a setter/getter are not used.
+  t_getter getter;  // the function which gets the value (wherever it may be).
+  t_setter setter;  // the function which sets the value.
+  bool changed;
 public:
-    VariableBinding(const char* name, T init_value);
-    VariableBinding(const char* name, t_getter getter, t_setter setter);
-    VariableBinding(const char* name, T* t_ptr);
-    void setValue(T val);             // function pointer to set the real value of this variable.
-    T getValue();                     // function pointer to get the real value of this variable.
-    void setValue(T* val);            // function pointer to set the real value of this variable.
-    double getNumRep();               // get the numerical (double) representation of what this variable is bound to.
-                                      // this function might just be a wrapper for (double) casting (which will fail
-                                      // if T is of an incorrect type)
-    char* getStringRep();             // get the string representation of what this variable is bound to.
+  VariableBinding(const char* name, T init_value);
+  VariableBinding(const char* name, t_getter getter, t_setter setter);
+  VariableBinding(const char* name, T* t_ptr);
+  void setValue(T val);             // set the real value of this variable.
+  void setValue(T* val);            // set the real value of this variable.
+  T getValue();                     // get the real value of this variable.
+
+  double getNumRep();               // get the numerical (double) representation
+                                    // of what this variable is bound to. This
+                                    // function may very well just be a wrapper
+                                    // for (double) casting (which will fail if
+                                    // T is of an incorrect type)
+  char* getStringRep();             // get the string representation of what this
+                                    // variable is bound to.
 };
 
 
 /*
  * This class is the supertype of all GUIElements.
- * It is not a template, so that it can be used as
+ * It is not templated, so that it can be used as
  * a type for lists.
  */
 class GenericGUIElement {
-    virtual void updateGUIFromBinding() =0;    // these two functions will have to be specialized for each supported
-    virtual void updateBindingFromGUI() =0;    //   window type. 
-    virtual CEGUI::Window* getWindow()  =0;    // Return the window of this class
-    virtual const char* getWindowType() =0;    // All CEGUI::Windows contain a member type string. Just return that.
+  virtual void updateGUIFromBinding() =0;   // these two functions will have to be
+  virtual void updateBindingFromGUI() =0;   // specialized for each supported
+                                            // window type. 
+  virtual CEGUI::Window* getWindow()  =0;   // Return the window of this class
+
+  virtual const char* getWindowType() =0;   // All CEGUI::Windows contain a member
+                                            // type string. Just return that.
 };
 
 /*
- * A mapping between Variable Bindings and their corresponding GUI elements (Ie. CEGUI::Windows).
- * W should be a subclass of CEGUI::Window. T can be anything.
+ * A mapping between Variable Bindings and their corresponding GUI elements
+ * (Ie. CEGUI::Windows). W should be a subclass of CEGUI::Window. T can be anything.
  */
 template<class W, class T>
 class GUIElement : public GenericGUIElement{
@@ -92,39 +85,47 @@ public:
 class HaleGUI{ 
 public:
     static HaleGUI* inst;
-    HaleGUI();
 protected:
     std::vector<GenericGUIElement> guiElements;
-public:
-
+    HaleGUI();
     ~HaleGUI();
-    HaleGUI* getInstance();
-    void addGUIElement(GenericGUIElement* in);         // Set up this Window: register input handlers, etc.
-    GenericGUIElement* removeGUIElement(GenericGUIElement* in);      // Clean up (but do not free memory for) this window: unregister input handlers, etc.
-    void removeGUIElement(char* varname);              // 
-    void renderAll();                                  // Render all CEGUI elements.
-    void init();                                       // Initialize CEGUI.
+public:
+  HaleGUI* getInstance();
+  // Set up this Window: register input handlers, etc.
+  void addGUIElement(GenericGUIElement* in);
 
-    // returns whether the specified value has been changed in a manner visible to its VariableBinding.
-    bool hasChanged(const char* name);
+  // Clean up (but do not free memory for) this GUIElement: unregister input handlers, etc.
+  GenericGUIElement* removeGUIElement(GenericGUIElement* in);
 
-    // returns whether any of the variables have changed.
-    bool hasChanged();
+  // Remove and return a single GUIElement which is bound to the particular variable, or null.
+  GenericGUIElement* removeGUIElement(char* varname);
+  void renderAll();                      // Render all CEGUI elements.
+  void init();                           // Initialize CEGUI.
+
+  bool hasChanged(const char* name);     // returns whether the specified value has
+                                         // been changed in a manner visible to its
+                                         // VariableBinding.
+
+  // returns whether any of the variables have changed, ie. if a redraw is necessary.
+  bool hasChanged();
 
 
-    // a set of callback handlers which must be included manually.
-    void gui_charCallback(GLFWwindow* window, unsigned int char_pressed);
-    void gui_cursorPosCallback(GLFWwindow* window, double x, double y);
-    void gui_keyCallback(GLFWwindow* window, int key, int scan, int action, int mod);
-    void gui_mouseButtonCallback(GLFWwindow* window, int button, int state, int mod);
-    void gui_mouseWheelCallback(GLFWwindow* window, double x, double y);
-    void gui_windowResizedCallback(GLFWwindow* window, int width, int height);
-    void gui_errorCallback(int error, const char* message);
+  // a set of callback handlers which must be used manually by the calling code.
+  void gui_charCallback(GLFWwindow* window, unsigned int char_pressed);
+  void gui_cursorPosCallback(GLFWwindow* window, double x, double y);
+  void gui_keyCallback(GLFWwindow* window, int key, int scan, int action, int mod);
+  void gui_mouseButtonCallback(GLFWwindow* window, int button, int state, int mod);
+  void gui_mouseWheelCallback(GLFWwindow* window, double x, double y);
+  void gui_windowResizedCallback(GLFWwindow* window, int width, int height);
+  void gui_errorCallback(int error, const char* message);
 
 };
-
+/////////////////////////////////////////////////////////////////
+////
+///                     Example Usage:
+///
+//
 /*
-Example Usage:
 
 
 double getIsoVal(){
@@ -161,3 +162,4 @@ int main(){
 
 
 */
+
